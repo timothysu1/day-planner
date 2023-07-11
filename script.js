8dad4b09025214f77afc226b3a2cbf202985ecb2
Timothy <timsu100@gmail.com>
2023-07-10 17:32:42 -0700

    add current day and work hours

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var currentDay = $("#currentDay");
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -9,15 +11,28 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+
   // TODO: Add code to display the current date in the header of the page.
+function displayTime() {
+  var today = dayjs().format('dddd, MMMM DD, YYYY');
+  currentDay.text(today);
+}
+displayTime();
+//checks if the date is correct every minute
+setInterval(displayTime, 1000 * 60)
 });
