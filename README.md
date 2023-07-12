90b798b74ddc906b3722b537db62ddd6efa012db
Timothy <timsu100@gmail.com>
2023-07-11 20:42:53 -0700

    Readme content

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Day Planner
+This webpage serves as a day scheduler that allows the user to log important events throughout the day. The user will be able to save these events so that they will reappear even if the user reloads or exits the page. 
 
+![Day Planner Website](./day-planner.png)
 
 ## Built With
 * [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
@@ -14,11 +16,13 @@
 * [See Live Site](https://timothysu1.github.io/day-planner/)
 
 ## Usage
-
+The user will be shown a page that displays the current date and time blocks that go from 9am to 5pm. The color of the block will reflect whether its the current block, a block that alreay happened, or a future block. The user will also be able to save the event they input by clicking on the save button to the right. Each save button will its corresponding block.
 
 
 ## Learning Points 
-* 
+* Utilizing Jquery, Bootstrap, and DayJS
+* Understanding *this* in the context where it is used
+* Changing attributes of elements using Java Script
 
 ## Important Code
 
@@ -32,7 +36,7 @@
   }
 ```
 
-
+This code allows the user to store what event they put into an hour block when they click its corresponding save button.
 
 
 ## Author Info
@@ -44,7 +48,7 @@
 
 
 ## Credits
-
+JQuery Documentation: https://api.jquery.com/
 
 ## License
 
--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 var currentDay = $("#currentDay");
 var saveBtn = $(".saveBtn");
-var hour = $(".hour");
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
+  
   //When button is clicked
   saveBtn.on("click", saveEvent)
   //Input for corresponding button is saved to local storage
   function saveEvent() {
     var targetHour = $(this).parent();
     var textArea = $(this).parent().find('.description').val();
-    console.log(targetHour.attr("id"));
     localStorage.setItem(targetHour.attr("id"), textArea);
   }
 
@@ -43,12 +31,6 @@ $(function () {
   }
   hourColor();
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
   //only need to iterate through 9am - 5pm
   function retrieveEvent() {
     for (var i = 9; i < 18; i++) {
@@ -58,14 +40,12 @@ $(function () {
   }
   retrieveEvent();
 
-
   // TODO: Add code to display the current date in the header of the page.
   function displayTime() {
     var today = dayjs().format('dddd, MMMM DD, YYYY');
     currentDay.text(today);
   }
   displayTime();
-
 
   //checks if the date and time is correct every minute
   setInterval(function(){
